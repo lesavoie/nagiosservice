d2424902e869615d7e928031bfaa5e9d8aaa2b9d
Lee Savoie <lee.savoie@gmail.com>
2014-03-21 18:27:25 -0700

    Add relationships between models

--- controlserver/servicelevelinterface/views.py
+++ controlserver/servicelevelinterface/views.py
@@ -17,22 +17,36 @@ class ContactViewSet(viewsets.ModelViewSet):
    # Another option is decopling this completely and just putting triggers on
    # the database.
    def list(self, request):
+      print 'List'
       return super(ContactViewSet, self).list(request)
 
    def create(self, request):
+      print 'Create'
       return super(ContactViewSet, self).create(request)
 
    def retrieve(self, request, pk=None):
+      print 'Retrieve'
       return super(ContactViewSet, self).retrieve(request, pk)
 
    def update(self, request, pk=None):
+      print 'Update'
       return super(ContactViewSet, self).update(request, pk)
 
    def partial_update(self, request, pk=None):
+      print 'Partial update'
       return super(ContactViewSet, self).partial_update(request, pk)
 
    def destroy(self, request, pk=None):
+      print 'Destroy'
       return super(ContactViewSet, self).destroy(request, pk)
+      
+   def post_save(self, obj, created=False):
+      print "Post Save"
+      return super(ContactViewSet, self).post_save(obj, created)
+      
+   def post_delete(self, obj):
+      print "Post Delete"
+      return super(ContactViewSet, self).post_delete(obj)
 
 
 class CommandViewSet(viewsets.ModelViewSet):
