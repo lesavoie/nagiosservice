42da3536d0f0e1689b1c9df9e2500c08f02c5472
Yi Huang <ethanhuang1991@gmail.com>
2014-04-21 16:34:26 -0700

    mapper: contact-group for each host/service implemented

--- controlserver/servicelevelinterface/mapperinterface.py
+++ controlserver/servicelevelinterface/mapperinterface.py
@@ -6,17 +6,34 @@ import subprocess
 class Mapper:
    single_command_template="define command{\n\tcommand_name\t%s\n\tcommand_line\t%s %s\n\t}\n"
    single_contact_template="define contact{\n\tcontact_name\t%s\n\tuse\tgeneric-contact\n\talias\t%s\n\temail\t%s\n\t}\n"
-   single_host_template="define host{\n\tuse\tgeneric-host\n\thost_name\t%s\n\talias\t%s\n\taddress\t%s\n\t}\n"
-   single_service_template="define service{\n\tuse\tgenaric-service\n\thost_name\t%s\n\tservice_description\t%s\n\tcheck_command\t%s\n\tmax_check_attempts\t%d\n\tnormal_check_interval\t%d\n\tnotification_interval\t%d\n\t}\n"
+   single_contactgroup_template="define contactgroup{\n\tcontactgroup_name\t%s\n\talias\t%s\n\tmembers\t%s\n\t}\n"
+   single_host_template="define host{\n\tuse\tgeneric-host\n\thost_name\t%s\n\talias\t%s\n\taddress\t%s\n\tmax_check_attempts\t%d\n\tcontact_groups\t%s\n\t}\n"
+   single_service_template="define service{\n\tuse\tgeneric-service\n\thost_name\t%s\n\tservice_description\t%s\n\tcheck_command\t%s\n\tmax_check_attempts\t%d\n\tnormal_check_interval\t%d\n\tcontact_groups\t%s\n\tnotification_interval\t%d\n\t}\n"
    
    def constructSingleContact(self, contact):
       snippet = (self.single_contact_template % (contact.contact_name, contact.human_readable_name,  contact.email_address))
       return snippet
+   
+   def constructContactgroup(self, name_prefix, contacts, contactsFile):
+      name = name_prefix
+      alias = ""
+      members = ""
+      if (type(contacts) is list):
+         contactList = contacts
+      else:
+         contactList = contacts.all()
+      for contact in contactList:
+         name += "_" + contact.contact_name
+         alias += contact.human_readable_name + "_"
+         members += contact.contact_name + ","
+      alias = alias[:-1]
+      members = members[:-1]
+      snippet = (self.single_contactgroup_template % (name, alias, members))
+      contactsFile.write(snippet)
+      return name
 
    # TODO: contact_groups 
-   def processMonitor(self, monitor, hostsFile, servicesFile):
-      host_str = (self.single_host_template % (monitor.monitor_name, monitor.human_readable_name, monitor.address))
-      hostsFile.write(host_str)
+   def processMonitor(self, monitor, hostsFile, servicesFile, contactsFile):
       
       # default max check attempts 3 times to determine its final(hard) state
       maxCheckAttempts = 3
@@ -37,7 +54,12 @@ class Mapper:
       # TODO: will need to change when we support command line parameters
       command_str = "checknrpe_1arg!" + monitor.command.command_name
       
-      service_str = (self.single_service_template % (monitor.monitor_name, monitor.command.command_name + " on " + monitor.monitor_name, command_str, maxCheckAttempts, checkInterval, notificationInterval))
+      contactgroups = self.constructContactgroup(monitor.monitor_name, monitor.contacts, contactsFile)
+      
+      host_str = (self.single_host_template % (monitor.monitor_name, monitor.human_readable_name, monitor.address, maxCheckAttempts, contactgroups))
+      hostsFile.write(host_str)
+      
+      service_str = (self.single_service_template % (monitor.monitor_name, monitor.command.command_name + " on " + monitor.monitor_name, command_str, maxCheckAttempts, checkInterval, contactgroups, notificationInterval))
       servicesFile.write(service_str)
 
    def do_map(self, user, monitors, contacts, commands):
@@ -48,15 +70,16 @@ class Mapper:
       servicesFile = open(parentDir + "services.cfg", "w")
       print "Mapping user:"
       print user
-      print "Monitors:"
-      print monitors
-      for monitor in monitors:
-         self.processMonitor(monitor, hostsFile, servicesFile)
       print "Contacts:"
       print contacts
       for contact in contacts:
          result = self.constructSingleContact(contact)
          contactsFile.write(result)
+      self.constructContactgroup("all-contact", contacts, contactsFile)
+      print "Monitors:"
+      print monitors
+      for monitor in monitors:
+         self.processMonitor(monitor, hostsFile, servicesFile, contactsFile)
       print "Commands:"
       print commands
       contactsFile.close()
