ad435c32d014e8b222f917a651ae35da718c4bfd
Yi Huang <ethanhuang1991@gmail.com>
2014-05-07 12:59:11 -0700

    mapper calls tools with only user token, without username

--- controlserver/servicelevelinterface/mapperinterface.py
+++ controlserver/servicelevelinterface/mapperinterface.py
@@ -80,7 +80,7 @@ class Mapper:
       # call tool (Timeout only works for python3.3 or this code won't compile)
       tokenStr = str(get_token(user))
       print "User Token:" + tokenStr
-      ret = subprocess.call(["ns_tools -t 1 -c 127.0.0.1 -p 5600 -i " + tokenStr + user.username + " -f " + parentDir + "cfgFile.cfg"], shell=True)
+      ret = subprocess.call(["ns_tools -t 1 -c 127.0.0.1 -p 5600 -i " + tokenStr + " -f " + parentDir + "cfgFile.cfg"], shell=True)
       
       # clean up temp folder
       shutil.rmtree(parentDir)
